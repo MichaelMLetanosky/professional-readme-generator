8083686eadbff9031dbe89660eb3ad92c96b8fdb
Michael Letanosky <MichaelMLetanosky@gmail.com>
2022-04-20 12:12:54 -0400

    Added additional TODOs

--- index.js
+++ index.js
@@ -8,14 +8,23 @@ const questions = [
     type: `input`,
     message: `What is your project's name?`,
     name: `projectName`,
+    // TODO: Add questions for description, installation instructions, usage information, contribution guidelines, and test instructions
+    // TODO: Add a list question for liscence information
+    // TODO: Add questions for gitHub and email contact ino
     },
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     fs.writeFile(`./output/README.md`, 
-        //TODO Include Answers
+        // TODO: Include Answers in a README mock up
+        // TODO: Include Title with Project Name
+        // TODO: Include linking table of contents
+        // TODO: Include license bridge
+        // TODO: Include sections for description, installation instructions, usage information, contribution guidelines, and test instructions
         `${data.projectName}`
+        // TODO: Include generateMarkdown for license stuff
+        // TODO: Add questions section with contact info
         , 
         (error) => error ? console.log(err) : console.log(`Success!`))
 }
