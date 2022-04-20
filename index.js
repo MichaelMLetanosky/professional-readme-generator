3cf5a2e135f0054543e163a87508f03149918175
Michael Letanosky <MichaelMLetanosky@gmail.com>
2022-04-20 12:00:17 -0400

    Added boilerplate and outline

---
+++ index.js
@@ -0,0 +1,32 @@
+// TODO: Include packages needed for this application
+var fs = require(`fs`);
+var inquirer = require(`fs`);
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+    type: `input`,
+    message: `What is your project's name?`,
+    name: `projectName`,
+    },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(`./output/README.md`, 
+        //TODO Include Answers
+        , 
+        (error) => error ? console.log(err) : console.log(`Success!`))
+  })
+}
+
+// TODO: Create a function to initialize app
+function init() {
+  inquirer
+    .prompt(questions)
+    .then(writeToFile(ReamMe, answers)
+    .catch((e) => {if (err) throw err;});
+}
+
+// Function call to initialize app
+init();
